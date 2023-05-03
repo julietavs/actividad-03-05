020bf5d059e733b135f4d0b6f8e53d824cc778b9
julietavs <127440539+julietavs@users.noreply.github.com>
2023-05-03 16:27:34 -0500

    Add files via upload

---
+++ trabajo3_05/script/script.js
@@ -0,0 +1,18 @@
+let increment = 0;
+
+let show = document. querySelector('.show')
+let cuerpo = document.querySelector('.container')
+
+document.addEventListener('click', (e)=>{
+    console.log(e)
+
+    if(e.target.matches('#decrease')){
+        increment--
+        show.innerHTML = increment
+    }
+} )
+
+
+
+
+
